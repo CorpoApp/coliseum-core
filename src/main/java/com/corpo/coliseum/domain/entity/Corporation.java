6ac2e1f91611b85491feedb6104db0146f23ba3a
Thibaut STRECKER <thibaut.strecker@gmail.com>
2021-01-29 21:05:46 +0100

    fix id : change panacheEntity to panacheBaseEntity and set uuid as id

--- src/main/java/com/corpo/coliseum/domain/entity/Corporation.java
+++ src/main/java/com/corpo/coliseum/domain/entity/Corporation.java
@@ -1,27 +1,32 @@
 package com.corpo.coliseum.domain.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "corporation")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Corporation extends PanacheEntity {
+public class Corporation extends PanacheEntityBase {
 
+    @Id
+    public UUID id;
     @Column(unique = true)
     public String name;
     public String sport;
     @ManyToMany
+    @JoinTable(
+            name = "corporation_users",
+            joinColumns = @JoinColumn(name = "corporation_id"),
+            inverseJoinColumns = @JoinColumn(name = "corpo_user_id")
+    )
     public List<User> userList;
 }
 
