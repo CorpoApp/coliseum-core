2db2fc54b26ccfeb61256d56d11374087a334a01
Thibaut STRECKER <thibaut.strecker@gmail.com>
2021-01-29 21:08:25 +0100

    Fix wildcard import

--- src/main/java/com/corpo/coliseum/domain/entity/Corporation.java
+++ src/main/java/com/corpo/coliseum/domain/entity/Corporation.java
@@ -5,7 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.List;
 import java.util.UUID;
 
